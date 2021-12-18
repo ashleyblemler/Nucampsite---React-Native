c85aa8237c7600283cb1bc29284ed1dc4a5098f3
Ashley Blemler <ashleyblemler@gmail.com>
2021-12-18 12:31:33 -0800

    Week 1 Workshop - Updated

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -13,7 +13,7 @@ class Contact extends Component {
             <ScrollView>
                 <Card
                     title="Contact Information"
-                    wrapperStyle={{margin: 10}}
+                    wrapperStyle={{margin: 20}}
                 >
                     <Text>1 Nucamp Way</Text>
                     <Text>Seattle, WA 98001</Text>
